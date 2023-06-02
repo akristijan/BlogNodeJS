a073e9884f97d86f31c881e652cb05e0d21f571f
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2023-06-02 21:59:51 +0200

    finished auth

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require("../controllers/admin")
+const ensureAuth = require("../controllers/auth")
 
 //Get Admin - Login Page
 router.get("/", adminController.getAdmin);
 router.post("/", adminController.checkLogin);
-router.get('/dashboard', adminController.getDashboard);
+router.get('/dashboard', ensureAuth, adminController.getDashboard);
 //Sign up a new user
 router.post('/register', adminController.registerUser)
 
